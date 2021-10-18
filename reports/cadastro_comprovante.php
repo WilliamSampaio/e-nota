70b37fbae7de76bc7a18b6725751b5bd213f8c41
William Sampaio <1997wbms@gmail.com>
2021-10-18 09:33:14 -0400

    fix reports

--- reports/cadastro_comprovante.php
+++ reports/cadastro_comprovante.php
@@ -19,75 +19,90 @@ Fith Floor, Boston, MA 02110-1301, USA
 */
 ?>
 <?php
-	include("../include/conect.php");
-	include("../funcoes/util.php");
-	$codigo = base64_decode($_GET["COD"]);
-	
-	
-	
-	// busca os dados do municipio
-	$sql = mysql_query("SELECT cidade, secretaria, brasao FROM configuracoes");
-	list($CIDADE,$SECRETARIA,$BRASAO) = mysql_fetch_array($sql);
-	
-	// busca os dados da inst. financeira
-	$sql_inst = mysql_query("SELECT IF(cpf IS NULL,cnpj,cpf) AS 'cnpj', razaosocial, logradouro, numero, municipio, uf FROM cadastro WHERE codigo='$codigo'");
-	list($cnpj,$razaosocial,$logradouro,$numero,$municipio,$uf) = mysql_fetch_array($sql_inst);
-	
+include("../include/conect.php");
+include("../funcoes/util.php");
+$codigo = base64_decode($_GET["COD"]);
+
+
+
+// busca os dados do municipio
+$sql = $PDO->query("SELECT cidade, secretaria, brasao FROM configuracoes");
+list($CIDADE, $SECRETARIA, $BRASAO) = $sql->fetch();
+
+// busca os dados da inst. financeira
+$sql_inst = $PDO->query("SELECT IF(cpf IS NULL,cnpj,cpf) AS 'cnpj', razaosocial, logradouro, numero, municipio, uf FROM cadastro WHERE codigo='$codigo'");
+list($cnpj, $razaosocial, $logradouro, $numero, $municipio, $uf) = $sql_inst->fetch();
+
 ?>
 <title>Comprovante de Cadastro</title>
 <div id="imprimir" align="center">
- <input type="button" onclick="document.getElementById('imprimir').style.display='none'; print(); document.getElementById('imprimir').style.display='block';" value="Imprimir" />
+  <input type="button" onclick="document.getElementById('imprimir').style.display='none'; print(); document.getElementById('imprimir').style.display='block';" value="Imprimir" />
 </div>
 <table width="800" border="0" cellspacing="0" cellpadding="5" align="center">
   <tr>
     <td height="100" colspan="4">
-	<table border="0" cellspacing="0" cellpadding="5" style="border: 0px;">
-      <tr>
-        <td width="150" style="border:0px;" align="left"><?php if($CONF_BRASAO){?><img src="../img/brasoes/<?php echo rawurlencode($CONF_BRASAO); ?>" width="100" height="100"><?php }?></td>
-        <td width="520" style="border:0px;" align="left" valign="middle">
-		<font class="prefeitura">Prefeitura Municipal de <?php echo $CONF_CIDADE; ?></font><br>
-		<font class="secretaria"><?php echo $CONF_SECRETARIA; ?><br>
-		Comprovante de Cadastro de Empresa </font></td>
-      </tr>
-    </table></td>
+      <table border="0" cellspacing="0" cellpadding="5" style="border: 0px;">
+        <tr>
+          <td width="150" style="border:0px;" align="left"><?php if ($CONF_BRASAO) { ?><img src="../img/brasoes/<?php echo rawurlencode($CONF_BRASAO); ?>" width="100" height="100"><?php } ?></td>
+          <td width="520" style="border:0px;" align="left" valign="middle">
+            <font class="prefeitura">Prefeitura Municipal de <?php echo $CONF_CIDADE; ?></font><br>
+            <font class="secretaria"><?php echo $CONF_SECRETARIA; ?><br>
+              Comprovante de Cadastro de Empresa </font>
+          </td>
+        </tr>
+      </table>
+    </td>
   </tr>
   <tr>
     <td align="center" width="50%" bgcolor="#CCCCCC" colspan="2"><strong>N&Uacute;MERO DO DOCUMENTO</strong></td>
     <td align="center" bgcolor="#CCCCCC" colspan="2"><strong>DATA DE EMISS&Atilde;O </strong></td>
   </tr>
   <tr>
-    <td align="center" colspan="2" width="50%"><font class="prefeitura"><?php echo $codigo ?></font></td>
-    <td align="center" colspan="2"><font class="prefeitura"><?php echo DataPtExt(); ?></font></td>
+    <td align="center" colspan="2" width="50%">
+      <font class="prefeitura"><?php echo $codigo ?></font>
+    </td>
+    <td align="center" colspan="2">
+      <font class="prefeitura"><?php echo DataPtExt(); ?></font>
+    </td>
   </tr>
   <tr>
     <td height="30" colspan="4" align="center" bgcolor="#CCCCCC"><strong>IDENTIFICA&Ccedil;&Atilde;O DO SUJEITO PASSIVO </strong></td>
   </tr>
   <tr>
-    <td height="50" colspan="3" valign="top">Razão Social:<br>
-    <font class="prefeitura"><?php echo $razaosocial; ?></font></td>
+    <td height="50" colspan="3" valign="top">Razï¿½o Social:<br>
+      <font class="prefeitura"><?php echo $razaosocial; ?></font>
+    </td>
     <td width="25%" valign="top">CNPJ/CPF<br>
-    <font class="prefeitura"><?php echo $cnpj; ?></font></td>
+      <font class="prefeitura"><?php echo $cnpj; ?></font>
+    </td>
   </tr>
   <tr>
     <td height="75" colspan="4" valign="top">Endere&ccedil;o<br>
-    <font class="prefeitura"><?php echo "$logradouro, $numero, $municipio, $uf"; ?></font></td>
+      <font class="prefeitura"><?php echo "$logradouro, $numero, $municipio, $uf"; ?></font>
+    </td>
   </tr>
   <tr>
     <td height="30" colspan="4" align="center" bgcolor="#CCCCCC"><strong>CERTIFICA&Ccedil;&Atilde;O</strong></td>
   </tr>
   <tr>
-    <td height="200" colspan="4" align="center" valign="middle"><div class="style1">
-      <blockquote>A Prefeitura Municipal de <font class="prefeitura"><?php echo $CONF_CIDADE; ?></font> certifica que a Empresa citada acima foi devidamente cadastrada no sistema de NF-e do município.</blockquote>
-    </div>   </td>
+    <td height="200" colspan="4" align="center" valign="middle">
+      <div class="style1">
+        <blockquote>A Prefeitura Municipal de <font class="prefeitura"><?php echo $CONF_CIDADE; ?></font> certifica que a Empresa citada acima foi devidamente cadastrada no sistema de NF-e do municï¿½pio.</blockquote>
+      </div>
+    </td>
   </tr>
   <tr>
     <td height="30" colspan="4" align="center" bgcolor="#CCCCCC"><strong>OBSERVA&Ccedil;&Otilde;ES</strong></td>
   </tr>
   <tr>
-    <td colspan="4"><p>- A senha de acesso da Empresa ao sistema de NF-e do município é de uso exclusivo e intransferível da Empresa, bem como a responsabilidade sobre o uso indevido da mesma.
-    </p></td>
+    <td colspan="4">
+      <p>- A senha de acesso da Empresa ao sistema de NF-e do municï¿½pio ï¿½ de uso exclusivo e intransferï¿½vel da Empresa, bem como a responsabilidade sobre o uso indevido da mesma.
+      </p>
+    </td>
   </tr>
   <tr>
-    <td height="50" colspan="4"><font class="prefeitura"><?php ?></font>.  </td>
+    <td height="50" colspan="4">
+      <font class="prefeitura"><?php ?></font>.
+    </td>
   </tr>
 </table>
--- reports/certpagamento.php
+++ reports/certpagamento.php
@@ -18,29 +18,29 @@ www.softwarepublico.gov.br, ou escreva para a Fundacao do Software Livre Inc., 5
 Fith Floor, Boston, MA 02110-1301, USA
 */
 ?>
-<?php 
+<?php
 session_start();
 include("../include/conect.php");
 include("../funcoes/util.php");
-if (!$_GET['CODV']){
-	Mensagem(" COD Inválido");
+if (!$_GET['CODV']) {
+	Mensagem(" COD Invï¿½lido");
 	Redireciona("../site/certidoes.php");
 }
-if (!$_SESSION['SESSAO_cnpj_emissor']){
-	Mensagem(" CNPJ Inválido");
+if (!$_SESSION['SESSAO_cnpj_emissor']) {
+	Mensagem(" CNPJ Invï¿½lido");
 	Redireciona("../site/certidoes.php");
 }
 
-$cnpjcpf_empresa =$_SESSION['SESSAO_cnpj_emissor'];
+$cnpjcpf_empresa = $_SESSION['SESSAO_cnpj_emissor'];
 $codverif = base64_decode($_GET['CODV']);
 
-$sql_banco = mysql_query (" SELECT bancos.banco
+$sql_banco = $PDO->query(" SELECT bancos.banco
 							FROM bancos
 							INNER JOIN boleto ON bancos.codigo = boleto.codbanco");
 
-list($nome_banco) = mysql_fetch_array($sql_banco);
+list($nome_banco) = $sql_banco->fetch();
 
-$sql=mysql_query("
+$sql = $PDO->query("
 			SELECT 
 				certidoes_pagamento.codigo,
 				certidoes_pagamento.codverificacao,
@@ -56,26 +56,26 @@ $sql=mysql_query("
 				emissores ON certidoes_pagamento.codemissor=emissores.codigo
 			WHERE 
 				emissores.cnpjcpf='$cnpjcpf_empresa' AND
-				certidoes_pagamento.codigo = '$codverif'");	
+				certidoes_pagamento.codigo = '$codverif'");
 
-list($nrodoc,$codverificacao,$certemissao,$certvalidade,$nomeempresa,$cnpjcpf,$endereco,$codemissor)=mysql_fetch_array($sql);
-$temp_emissao = explode("-",$certemissao);
+list($nrodoc, $codverificacao, $certemissao, $certvalidade, $nomeempresa, $cnpjcpf, $endereco, $codemissor) = $sql->fetch();
+$temp_emissao = explode("-", $certemissao);
 $anoemissao = $temp_emissao[0];
 $anoemissao--;
-$certemissao2 = $anoemissao."-".$temp_emissao[1]."-".$temp_emissao[2];
+$certemissao2 = $anoemissao . "-" . $temp_emissao[1] . "-" . $temp_emissao[2];
 
 $tomador_CNPJ = $cnpjcpf_empresa;
 
 //verifica se tem o emissor cadastrado
-$sql_emissor=mysql_query("SELECT codigo, nome, razaosocial, inscrmunicipal, endereco, municipio, uf, email 
+$sql_emissor = $PDO->query("SELECT codigo, nome, razaosocial, inscrmunicipal, endereco, municipio, uf, email 
 				  FROM emissores 
 				  WHERE cnpjcpf='$tomador_CNPJ'");
-if(mysql_num_rows($sql_emissor)) {
+if ($sql_emissor->rowCount()) {
 	$resultado = "s";
 	$existe_emissor = "s";
 }
 
-$sql_tomador = mysql_query ("SELECT codigo, 
+$sql_tomador = $PDO->query("SELECT codigo, 
 									nome, 
 									inscrmunicipal, 
 									endereco, 
@@ -84,14 +84,14 @@ $sql_tomador = mysql_query ("SELECT codigo,
 									email 
 							FROM tomadores 
 							WHERE cnpjcpf='$tomador_CNPJ'");
-if (mysql_num_rows($sql_tomador)){
+if ($sql_tomador->rowCount()) {
 	$resultado = "s";
 	$existe_tomador = "s";
 }
 
-if ($resultado){
-	
-	if ($existe_emissor){
+if ($resultado) {
+
+	if ($existe_emissor) {
 		$stringSql[0] = ("(
 			SELECT
 				guia_pagamento.codigo, 
@@ -111,7 +111,7 @@ if ($resultado){
 				guia_pagamento.pago = 'S'  AND
 				emissores.cnpjcpf = '$tomador_CNPJ' AND
 				guias_declaracoes.relacionamento = 'des'
-		)"); 
+		)");
 	}
 	if ($existe_tomador) {
 		$stringSql[2] = ("
@@ -132,101 +132,118 @@ if ($resultado){
 			WHERE
 			  guia_pagamento.pago = 'S'  AND
 			  tomadores.cnpjcpf = '$tomador_CNPJ' AND
-			  guias_declaracoes.relacionamento = 'des_issretido')"); 
+			  guias_declaracoes.relacionamento = 'des_issretido')");
 	}
-	$string_Sql = implode(" UNION ",$stringSql)." ORDER BY datavencimento";
-	$sql_guias = mysql_query($string_Sql);
+	$string_Sql = implode(" UNION ", $stringSql) . " ORDER BY datavencimento";
+	$sql_guias = $PDO->query($string_Sql);
 }
 
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<title>Certid&atilde;o de Pagamento de ISS</title>
-<link href="../css/imprimir_certidoes.css" rel="stylesheet" type="text/css">
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<title>Certid&atilde;o de Pagamento de ISS</title>
+	<link href="../css/imprimir_certidoes.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
-<table width="800" border="0" cellspacing="0" cellpadding="5" align="center">
-  <tr>
-    <td height="100" colspan="4">
-	<table border="0" cellspacing="0" cellpadding="5" style="border: 0px;">
-      <tr>
-        <td width="150" style="border:0px;" align="left"><?php if($CONF_BRASAO){?><img src="../img/brasoes/<?php echo rawurlencode($CONF_BRASAO); ?>"><?php }?></td>
-        <td width="520" style="border:0px;" align="left" valign="middle">
-		<font class="prefeitura">Prefeitura Municipal de <?php echo $CONF_CIDADE; ?></font><br>
-		<font class="secretaria"><?php echo $CONF_SECRETARIA; ?><br>
-		Demonstrativo de Pagamentos do ISS		</font></td>
-      </tr>
-    </table></td>
-  </tr>
-  <tr>
-    <td width="25%" height="30" align="center" bgcolor="#CCCCCC"><strong>Documento N&ordm;.</strong></td>
-    <td width="25%" align="center" bgcolor="#CCCCCC"><strong>C&oacute;digo de Verifica&ccedil;&atilde;o </strong></td>
-    <td width="25%" align="center" bgcolor="#CCCCCC"><strong>Data de Emiss&atilde;o </strong></td>
-    <td width="25%" align="center" bgcolor="#CCCCCC"><strong>Data de Validade</strong></td>
-  </tr>
-  <tr>
-    <td height="30" align="center"><font class="prefeitura"><?php echo $nrodoc; ?></font></td>
-    <td align="center"><font class="prefeitura"><?php echo $codverificacao; ?></font></td>
-    <td align="center"><font class="prefeitura"><?php echo DataPt($certemissao); ?></font></td>
-    <td align="center"><font class="prefeitura"><?php echo DataPt($certvalidade); ?></font></td>
-  </tr>
-  <tr>
-    <td height="30" colspan="4" align="center" bgcolor="#CCCCCC"><strong>IDENTIFICA&Ccedil;&Atilde;O DO SUJEITO PASSIVO </strong></td>
-  </tr>
-  <tr>
-    <td height="50" colspan="3" valign="top">Nome<br>
-    <font class="prefeitura"><?php echo $nomeempresa; ?></font></td>
-    <td valign="top">CNPJ/CPF<br>
-    <font class="prefeitura"><?php echo $cnpjcpf; ?></font></td>
-  </tr>
-  <tr>
-    <td height="75" colspan="4" valign="top">Endere&ccedil;o<br>
-    <font class="prefeitura"><?php echo $endereco; ?></font></td>
-  </tr>
-  <tr>
-    <td height="30" colspan="4" align="center" bgcolor="#CCCCCC"><strong>PAGAMENTOS EFETUADOS </strong></td>
-  </tr>
-  <tr>
-    <td height="20" align="center" valign="middle"><strong>Guia</strong></td>
-    <td height="20" align="center" valign="middle"><strong>Data</strong></td>
-    <td height="20" align="center" valign="middle"><strong>Banco</strong></td>
-    <td height="20" align="center" valign="middle"><strong>Valor Pago (R$) </strong></td>
-  </tr>
-	<?php 
-	while(list($codigo_guia, $dataemissao_guia, $valor_guia, $datavencimento_guia, $pago_guia) = mysql_fetch_array($sql_guias)){
-  		$valor_total += $valor_guia;
+	<table width="800" border="0" cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td height="100" colspan="4">
+				<table border="0" cellspacing="0" cellpadding="5" style="border: 0px;">
+					<tr>
+						<td width="150" style="border:0px;" align="left"><?php if ($CONF_BRASAO) { ?><img src="../img/brasoes/<?php echo rawurlencode($CONF_BRASAO); ?>"><?php } ?></td>
+						<td width="520" style="border:0px;" align="left" valign="middle">
+							<font class="prefeitura">Prefeitura Municipal de <?php echo $CONF_CIDADE; ?></font><br>
+							<font class="secretaria"><?php echo $CONF_SECRETARIA; ?><br>
+								Demonstrativo de Pagamentos do ISS </font>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td width="25%" height="30" align="center" bgcolor="#CCCCCC"><strong>Documento N&ordm;.</strong></td>
+			<td width="25%" align="center" bgcolor="#CCCCCC"><strong>C&oacute;digo de Verifica&ccedil;&atilde;o </strong></td>
+			<td width="25%" align="center" bgcolor="#CCCCCC"><strong>Data de Emiss&atilde;o </strong></td>
+			<td width="25%" align="center" bgcolor="#CCCCCC"><strong>Data de Validade</strong></td>
+		</tr>
+		<tr>
+			<td height="30" align="center">
+				<font class="prefeitura"><?php echo $nrodoc; ?></font>
+			</td>
+			<td align="center">
+				<font class="prefeitura"><?php echo $codverificacao; ?></font>
+			</td>
+			<td align="center">
+				<font class="prefeitura"><?php echo DataPt($certemissao); ?></font>
+			</td>
+			<td align="center">
+				<font class="prefeitura"><?php echo DataPt($certvalidade); ?></font>
+			</td>
+		</tr>
+		<tr>
+			<td height="30" colspan="4" align="center" bgcolor="#CCCCCC"><strong>IDENTIFICA&Ccedil;&Atilde;O DO SUJEITO PASSIVO </strong></td>
+		</tr>
+		<tr>
+			<td height="50" colspan="3" valign="top">Nome<br>
+				<font class="prefeitura"><?php echo $nomeempresa; ?></font>
+			</td>
+			<td valign="top">CNPJ/CPF<br>
+				<font class="prefeitura"><?php echo $cnpjcpf; ?></font>
+			</td>
+		</tr>
+		<tr>
+			<td height="75" colspan="4" valign="top">Endere&ccedil;o<br>
+				<font class="prefeitura"><?php echo $endereco; ?></font>
+			</td>
+		</tr>
+		<tr>
+			<td height="30" colspan="4" align="center" bgcolor="#CCCCCC"><strong>PAGAMENTOS EFETUADOS </strong></td>
+		</tr>
+		<tr>
+			<td height="20" align="center" valign="middle"><strong>Guia</strong></td>
+			<td height="20" align="center" valign="middle"><strong>Data</strong></td>
+			<td height="20" align="center" valign="middle"><strong>Banco</strong></td>
+			<td height="20" align="center" valign="middle"><strong>Valor Pago (R$) </strong></td>
+		</tr>
+		<?php
+		while (list($codigo_guia, $dataemissao_guia, $valor_guia, $datavencimento_guia, $pago_guia) = $sql_guias->fetch()) {
+			$valor_total += $valor_guia;
 		?>
-	  <tr>
-	    <td height="20" align="center" valign="middle"><?php echo $codigo_guia; ?></td>
-	    <td height="20" align="center" valign="middle"><?php echo DataPt($dataemissao_guia); ?></td>
-	    <td height="20" align="center" valign="middle"><?php echo $nome_banco; ?></td>
-	    <td height="20" align="center" valign="middle"><?php echo DecToMoeda($valor_guia); ?></td>
-	  </tr>
-		<?php 
-	}
-	?>
-  <tr>
-    <td height="20" colspan="3" align="right" valign="middle"><strong>Valor Total (R$)</strong>&nbsp;&nbsp;</td>
-    <td height="20" align="center" valign="middle"><?php echo DecToMoeda($valor_total); ?></td>
-  </tr>
-  <tr>
-    <td height="100" colspan="4" align="center" valign="middle">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="30" colspan="4" align="center" bgcolor="#CCCCCC"><strong>OBSERVA&Ccedil;&Otilde;ES</strong></td>
-  </tr>
-  <tr>
-    <td colspan="4"><p>- O presente documento somente tem validade:<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;a. Quando n&atilde;o apresentar rasuras;<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;b. At&eacute; a data de validade exposta acima;</p>
-    </td>
-  </tr>
-  <tr>
-    <td height="50" colspan="4">A aceita&ccedil;&atilde;o deste documento esta condicionada &agrave; verifica&ccedil;&atilde;o de sua validade, de forma exclusiva pelo aceitante junto &agrave; Prefeitura Municipal de <font class="prefeitura"><?php echo $CONF_CIDADE; ?></font>.  </td>
-  </tr>
-</table>
+			<tr>
+				<td height="20" align="center" valign="middle"><?php echo $codigo_guia; ?></td>
+				<td height="20" align="center" valign="middle"><?php echo DataPt($dataemissao_guia); ?></td>
+				<td height="20" align="center" valign="middle"><?php echo $nome_banco; ?></td>
+				<td height="20" align="center" valign="middle"><?php echo DecToMoeda($valor_guia); ?></td>
+			</tr>
+		<?php
+		}
+		?>
+		<tr>
+			<td height="20" colspan="3" align="right" valign="middle"><strong>Valor Total (R$)</strong>&nbsp;&nbsp;</td>
+			<td height="20" align="center" valign="middle"><?php echo DecToMoeda($valor_total); ?></td>
+		</tr>
+		<tr>
+			<td height="100" colspan="4" align="center" valign="middle">&nbsp;</td>
+		</tr>
+		<tr>
+			<td height="30" colspan="4" align="center" bgcolor="#CCCCCC"><strong>OBSERVA&Ccedil;&Otilde;ES</strong></td>
+		</tr>
+		<tr>
+			<td colspan="4">
+				<p>- O presente documento somente tem validade:<br>
+					&nbsp;&nbsp;&nbsp;&nbsp;a. Quando n&atilde;o apresentar rasuras;<br>
+					&nbsp;&nbsp;&nbsp;&nbsp;b. At&eacute; a data de validade exposta acima;</p>
+			</td>
+		</tr>
+		<tr>
+			<td height="50" colspan="4">A aceita&ccedil;&atilde;o deste documento esta condicionada &agrave; verifica&ccedil;&atilde;o de sua validade, de forma exclusiva pelo aceitante junto &agrave; Prefeitura Municipal de <font class="prefeitura"><?php echo $CONF_CIDADE; ?></font>. </td>
+		</tr>
+	</table>
 </body>
+
 </html>
