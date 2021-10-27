c65dbb50ee6fcd85aace5e6b7f78ee8523606f32
William Sampaio <1997wbms@gmail.com>
2021-10-27 11:17:56 -0400

    add breadcrumb

--- site/prestadores.php
+++ site/prestadores.php
@@ -39,6 +39,13 @@ require_once 'inc/header.php';
             <!-- CONTEÚDO -->
             <div class="col-sm-12 col-xl-9">
 
+                <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">Início</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Prestadores</li>
+                    </ol>
+                </nav>
+
                 <br>
                 <h1>Prestadores</h1>
                 <h5 class="card-title">Área destinada aos <strong>Prestadores de Serviços</strong> para acesso ao
