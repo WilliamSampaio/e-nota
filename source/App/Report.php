44a98cc733d5bfb16344e9a1c2de0629082697f9
WILLIAM B. SAMPAIO <56176702+WilliamSampaio@users.noreply.github.com>
2021-11-24 16:38:21 -0400

    reports controller

---
+++ source/App/Report.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Source\App;
+
+use League\Plates\Engine;
+use Source\Models\Configuracao;
+use Source\Models\Nota;
+
+class Report
+{
+    private $view;
+    private $data;
+    private $configuracoes;
+
+    public function __construct()
+    {
+        $this->view = new Engine(dirBase('views'));
+        $this->configuracoes = new Configuracao();
+        $this->data = [];
+    }
+
+    public function imprimirNota($data)
+    {
+        $this->data = array(
+            'title' => 'Imprimir Nota | ' . SITE,
+            'configuracoes' => $this->configuracoes->findById(1)
+        );
+
+        $cod_verificacao = base64_decode($data['nota']);
+        $nota = (new Nota())->find('cod_verificacao = :cod', "cod={$cod_verificacao}")->fetch();
+        
+        $this->data['nota'] = $nota->data();
+        $this->data['prestador'] = $nota->getPrestador()->data();
+        $this->data['tomador'] = $nota->getTomador()->data();
+        $this->data['servicos'] = $nota->getServicos();
+
+        echo $this->view->render('report/imprimir-nota', $this->data);
+    }
+}
